5b7dd16050a3864a4f3600c993721eefc0bbc0b1
samerr22 <laksameera5@gmail.com>
2023-12-10 15:13:27 +0530

    complete the sign in page functionility

--- client/src/pages/SigUp.jsx
+++ client/src/pages/SigUp.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+
 
 export default function SigUp() {
 const [formData, setFormData] = useState({});
 const [error, setError] = useState(false);
 const [loading, setLoading] = useState(false);
+const navigate = useNavigate();
   const handleChange = (e) =>{
     setFormData({...formData, [e.target.id]: e.target.value});
   }
@@ -30,6 +32,7 @@ const [loading, setLoading] = useState(false);
         setError(true);
         return;
       }
+      navigate('/sign-in');
       
       
     } catch (error) {
