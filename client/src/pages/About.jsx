9a6b53c87e06ed911542ea121d4de7bbb33f5289
samerr22 <laksameera5@gmail.com>
2023-12-12 16:12:03 +0530

    finshed home and about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,16 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className='px-4 py-12 max-w-2xl mx-auto'>
+    <h1 className='text-3xl font-bold mb-4 text-slate-700'>About</h1>
+    <p className='mb-4 text-slate-700'>This is a full-stack web application built with 
+      MEREN (mongodb, express, react, node.js) stack. it 
+      includes authentication features that allow users to 
+      sign up , log in, and log out, and provides access to 
+      protected routes only for authenticated users.
+    </p>
+
+
+  </div>
   )
 }
